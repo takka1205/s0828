0107a109fe613d57dd51e7f545188b26cf826173
石川貴洋 <>
2024-08-29 13:14:57 +0900

    Fix Prisma Client generation for Vercel5

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,17 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+import Cors from "cors";
 import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+// Initialize CORS
+const cors = Cors({
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  origin: "https://s0828.vercel.app", // ここで許可するオリジンを指定
+});
+
+// Helper function to run middleware
+function runMiddleware(req: NextRequest, res: NextResponse, fn: Function) {
+  return new Promise<void>((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 const prisma = new PrismaClient();
 
-export async function POST(req: Request) {
+export const GET = async (req: NextRequest) => {
+  // Run CORS middleware
+  await runMiddleware(req, NextResponse.next(), cors);
+
+  const { name, email } = await req.json();
   try {
-    const { name, email } = await req.json();
-    const user = await prisma.user.create({
-      data: { name, email },
-    });
-    return NextResponse.json({ message: "成功", user }, { status: 200 });
+    const users = await prisma.user.create({ data: { name, email } });
+    return NextResponse.json({ message: "成功", users }, { status: 200 });
   } catch (err) {
-    console.error(err);
     return NextResponse.json({ message: "失敗", err }, { status: 500 });
   }
-}
+};
