449e80dd46de36856d4b5e5b19b6f88e9b237b34
石川貴洋 <>
2024-08-29 13:22:17 +0900

    Fix Prisma Client generation for Vercel6

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,14 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
+import type { NextApiRequest, NextApiResponse } from "next";
 import Cors from "cors";
 import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 // Initialize CORS
 const cors = Cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  origin: "https://s0828.vercel.app", // ここで許可するオリジンを指定
+  origin: "https://s0828-h8ncp6313-takka1205s-projects.vercel.app", // 許可するオリジンを指定
+  credentials: true,
 });
 
 // Helper function to run middleware
-function runMiddleware(req: NextRequest, res: NextResponse, fn: Function) {
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
   return new Promise<void>((resolve, reject) => {
     fn(req, res, (result: any) => {
       if (result instanceof Error) {
@@ -19,17 +27,23 @@ function runMiddleware(req: NextRequest, res: NextResponse, fn: Function) {
   });
 }
 
-const prisma = new PrismaClient();
-
-export const GET = async (req: NextRequest) => {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   // Run CORS middleware
-  await runMiddleware(req, NextResponse.next(), cors);
+  await runMiddleware(req, res, cors);
 
-  const { name, email } = await req.json();
-  try {
-    const users = await prisma.user.create({ data: { name, email } });
-    return NextResponse.json({ message: "成功", users }, { status: 200 });
-  } catch (err) {
-    return NextResponse.json({ message: "失敗", err }, { status: 500 });
+  if (req.method === "POST") {
+    const { name, email } = req.body;
+    try {
+      const users = await prisma.user.create({ data: { name, email } });
+      return res.status(200).json({ message: "成功", users });
+    } catch (err) {
+      return res.status(500).json({ message: "失敗", err });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-};
+}
