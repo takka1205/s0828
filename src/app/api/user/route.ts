626323bdba0d94b96ab6aa0feaefde00edd3090c
石川貴洋 <>
2024-08-29 12:07:21 +0900

    Update some files or description of changes

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: Request) {
+  try {
+    const { name, email } = await req.json();
+    const user = await prisma.user.create({
+      data: { name, email },
+    });
+    return NextResponse.json({ message: "成功", user }, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { message: "失敗", error: err.message },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/thanks/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ThanksPage = () => {
+  return <div>ThanksPage</div>;
+};
+
+export default ThanksPage;
